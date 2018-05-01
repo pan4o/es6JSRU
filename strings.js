0facad0ab446ba37d1b04c0920d636607dcd9e35
pan4o <panchenko923@gmail.com>
2018-05-01 14:10:10 +0300

    strings

---
+++ strings.js
@@ -0,0 +1,25 @@
+
+
+let apples = 4;
+let oranges = 5;
+
+console.log(`${apples} + ${oranges} = ${apples + oranges}`)
+
+//Check if string has other strings
+
+let string = "hello tom";
+
+console.log(string.includes('tom')); // true
+
+//Check if string ends like other strings
+
+console.log(string.endsWith('llo tom')); //true
+
+//Check if string start like other strings
+
+console.log(string.startsWith('hell')); // true
+
+// Repeat string x'times
+let str = "test this ";
+
+console.log(str.repeat(3)); //test this test this test this 
